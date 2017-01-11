02c77d385972b1ee68693c799c4f016914bbcac1
Annastasia Psitos <annastasia.psitos@gmail.com>
2017-01-11 10:12:03 -0700

    begin

--- lib/components/Application.jsx
+++ lib/components/Application.jsx
@@ -1,10 +1,25 @@
 import React, { Component } from 'react'
 
-export default class Org extends Component {
+export default class Wish extends Component {
+  constructor() {
+    super();
+    this.state = {
+      wish: '',
+    }
+  }
+
+  componentDidMount() {
+    if 
+  }
+
+  wish(e) {
+    this.setState({ wish: e.target.value })
+  }
+
   render() {
     return (
       <div>
-        Oh Hey
+        <input type="text" onChange={(e) => {wish(e)}}/>
       </div>
     )
   }
